a5db0f983639140eeb2dcb46d71c20c0e501e7ea
ArtemKotlar <artemkotlar93@gmail.com>
2023-03-11 01:35:20 +0200

    зміни

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout/Layout';
+import { Layout } from './Layout';
 import { Toaster } from 'react-hot-toast';
-import { Sales } from 'Sales/Sales';
+import { Sales } from 'pages/Sales';
 import { Invoices } from './Invoice/Invoices';
 import { GlobalStyle } from './GlobalStyle';
+import { InvoiceDetails } from './Invoice/InvoiceDetails';
 
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<div>Homepage index route</div>} />
           <Route path="dashboard" element={<div>Dashboard</div>} />
           <Route path="sales" element={<Sales />}>
+            <Route index element={<div>Sales index route</div>} />
             <Route path="analytics" element={<div>Analytics</div>} />
-            <Route path="invoices" element={<Invoices />} />
+            <Route path="invoices" element={<Invoices />}>
+              <Route index element={<div>Invoices index route</div>} />
+              <Route path=":invoiceId" element={<InvoiceDetails />} />
+            </Route>
             <Route path="deposits" element={<div>Deposits</div>} />
           </Route>
           <Route path="reports" element={<div>Reports</div>} />
--- src/components/Invoice/Invoices.jsx
+++ src/components/Invoice/Invoices.jsx
@@ -2,6 +2,21 @@ import { Box } from 'components/Box';
 import { getInvoices } from 'fakeApi';
 import { useState, useEffect } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavItem = styled(NavLink)`
+  padding: ${p => p.theme.space[3]}px;
+  text-decoration: none;
+  color: ${p => p.theme.colors.text};
+  border-radius: 8px;
+  &.active {
+    background-color: ${p => p.theme.colors.primary};
+    color: ${p => p.theme.colors.white};
+  }
+  :hover:not(.active) {
+    color: ${p => p.theme.colors.primary};
+  }
+`;
 
 export const Invoices = () => {
   const [invoices, setInvoices] = useState([]);
@@ -9,14 +24,23 @@ export const Invoices = () => {
   useEffect(() => {
     getInvoices().then(setInvoices);
   }, []);
+
   return (
-    <div>
-      <Box as="aside" display="flex" flexDirection="column">
+    <Box display="flex">
+      <Box
+        as="aside"
+        display="flex"
+        flexDirection="column"
+        p={3}
+        borderRight="1px solid black"
+      >
         {invoices.map(({ id }) => (
-          <NavLink to={`${id}`}>{id}</NavLink>
+          <NavItem key={id} to={`${id}`}>
+            {id}
+          </NavItem>
         ))}
       </Box>
       <Outlet />
-    </div>
+    </Box>
   );
 };
